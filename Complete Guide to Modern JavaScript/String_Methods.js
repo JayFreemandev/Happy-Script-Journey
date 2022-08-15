23f5226bd57fce7a9a3dfb11bc5741488308c30d
JAY <jayfreemandev@gmail.com>
2022-08-15 17:38:05 +0900

    ES6 스트링 메서드

---
+++ Complete Guide to Modern JavaScript/String_Methods.js
@@ -0,0 +1,12 @@
+const str = "this is a potato";
+console.log(str.indexOf("potato"));
+
+const str2 = "this is a potato";
+console.log(str2.slice(0, 5));
+
+const str3 = "PIZZA";
+console.log(str3.startsWith("PIZZA"));
+console.log(str3.startsWith("CHICKEN"));
+
+const call = "주문완료";
+console.log(call.repeat(10));
