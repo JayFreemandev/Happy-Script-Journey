152dc99d8be550537c09dcd89c728f9e9f9eb454
jaeyun <jayfreemandev@gmail.com>
2022-09-15 00:43:12 +0900

    세트 메서드

---
+++ Complete Guide to Modern JavaScript/set2.js
@@ -0,0 +1,13 @@
+const pizza = new Set();
+
+pizza.add("potato");
+pizza.add("cheese");
+pizza.add("tomato");
+pizza.add("tomato");
+
+pizza.size;
+pizza.keys();
+pizza.entries();
+pizza.values();
+pizza.delete("tomato");
+pizza;
